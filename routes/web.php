dd5d51eb813dc926afc1dc85f8dfbe06aa631b81
徐大帅 <fibonacci701408733@163.com>
2020-12-15 20:24:20 +0800

    用户个人页面原型

--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,5 @@ Route::get('email/verify', 'Auth\VerificationController@show')->name('verificati
 Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
 Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
 
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+
