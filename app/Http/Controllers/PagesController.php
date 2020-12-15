64cf2e7bddaa6eef110f4dce464240e12abea926
徐大帅 <fibonacci701408733@163.com>
2020-12-15 19:38:46 +0800

    邮认证

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -8,6 +8,7 @@ class PagesController extends Controller
 {
     //
     public function root(){
+        //dd(\Auth::user()->hasVerifiedEmail());
         return view('pages.root');
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
-class User extends Authenticatable
+use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
+class User extends Authenticatable implements MustVerifyEmailContract
 {
-    use Notifiable;
+    use Notifiable, MustVerifyEmailTrait;
 
     /**
      * The attributes that are mass assignable.
