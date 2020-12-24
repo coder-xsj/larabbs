fe7625551db14dabb734b8ca1ef93d4160ac3629
徐大帅 <fibonacci701408733@163.com>
2020-12-24 22:46:19 +0800

    分类模型和数据

---
+++ app/Category.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Category extends Model
+{
+    //
+    use HasFactory;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'name', 'description'
+    ];
+}
