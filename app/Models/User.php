592ef4334f092798058f8c597afbd35efcb89a32
徐大帅 <fibonacci701408733@163.com>
2020-12-24 21:08:30 +0800

    用户上传头像

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable implements MustVerifyEmailContract
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'introduction',
+        'name', 'email', 'password', 'introduction', 'avatar',
     ];
 
     /**
