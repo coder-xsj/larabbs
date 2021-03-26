d0951735e46d3a8de35d33b1df98f9d2f9f8b36c
徐大帅 <fibonacci701408733@163.com>
2021-03-26 18:08:49 +0800

    Simditor编辑器

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -20,7 +20,7 @@ class TopicsController extends Controller
 
 	public function index(Request $request, Topic $topic)
 	{
-        $topics = $topic->withOrder($request->order)
+        $topics = $topic->withOrxder($request->order)
             ->with('user', 'category')  // 预加载防止 N+1 问题
             ->paginate(20);
         // $topics = Topic::with('user', 'category')->paginate(30);
--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+@section('styles')
+    <link rel="stylesheet" href="{{ asset('css/simditor.css') }}">
+@stop
 
     <div class="container">
         <div class="col-md-10 offset-md-1">
@@ -56,3 +59,17 @@
     </div>
 
 @endsection
+@section('scripts')
+    <script type="text/javascript" src="{{ asset('js/module.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/hotkeys.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/uploader.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/simditor.js') }}"></script>
+
+    <script>
+        $(document).ready(function() {
+            var editor = new Simditor({
+                textarea: $('#editor'),
+            });
+        });
+    </script>
+@stop
