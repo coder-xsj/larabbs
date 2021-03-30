e4b131b76b2deddf337f600023fa86cdb01414ab
徐大帅 <fibonacci701408733@163.com>
2021-03-30 23:52:28 +0800

    seo优化翻译

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,7 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Topic;
-
+use App\Handlers\SlugTranslateHandler;
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
@@ -20,10 +20,14 @@ class TopicObserver
     }
     // 存储话题的摘录
     public function saving(Topic $topic){
-        // 转义标签过滤
+        // 转义标签Xss过滤
         $topic->body = clean($topic->body, 'user_topic_body');
-
+        // 生成话题摘录
         $topic->excerpt = make_excerpt($topic->body);
+
+        if ((! $topic->slug))  {
+            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+        }
 
     }
 }
