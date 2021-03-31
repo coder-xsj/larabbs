c9dd6a439d7d0fc73fca07b6f933070e1123c3ec
徐大帅 <fibonacci701408733@163.com>
2021-03-31 19:40:22 +0800

    回复列表

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -26,13 +26,19 @@
         {{-- 用户发布的内容 --}}
         <div class="card ">
             <div class="card-body">
-                <div class="card-body">
-                    <ul class="nav nav-tabs">
-                        <li class="nav-item"><a class="nav-link active bg-transparent" href="#">Ta 的话题</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#">Ta 的回复</a></li>
-                    </ul>
+                <ul class="nav nav-tabs">
+                    <li class="nav-item">
+                        <a class="nav-link active bg-transparent {{ active_class(if_query('tab', null)) }}" href="{{ route('users.show', $user->id) }}">Ta 的话题</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link bg-transparent {{ active_class(if_query('tab', 'replies')) }}" href="{{ route('users.show', [$user->id, 'tab' => 'replies']) }}">Ta 的回复</a>
+                    </li>
+                </ul>
+                @if (if_query('tab', 'replies'))
+                    @include('users._replies', ['replies' => $user->replies()->with('topic')->recent()->paginate(5)])
+                @else
                     @include('users._topics', ['topics' => $user->topics()->recent()->paginate(5)])
-                </div>
+                @endif
             </div>
         </div>
 
