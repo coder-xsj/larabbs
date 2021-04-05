715c1d8a69b661d5cc2d3520ab6af8b54f1c06c2
徐大帅 <fibonacci701408733@163.com>
2021-04-05 15:18:21 +0800

    管理后台权限

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -12,4 +12,12 @@ class PagesController extends Controller
         return redirect()->route('topics.index');
 //        return view('pages.root');
     }
+    public function permissionDenied(){
+        // 检测用户是否有权限登录后台
+        if(config('administrator.permission')()){
+            return redirect(url(config('administrator.uri')),302);
+        }
+        // 否则使用视图
+        return view('pages.permission_denied');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -53,3 +53,6 @@ Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]
 
 // 通知类
 Route::resource('notifications', 'NotificationsController', ['only' => 'index']);
+
+// 后台拒绝路由
+Route::get('permission-denied', 'PagesController@permissionDenied')->name('permission-denied');
